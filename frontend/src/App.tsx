fb35ff7b5e58dca459ff1a35c0879b346b326b2f
gustavo-gsilva <prazeresdasilvag@gmail.com>
2026-03-04 16:06:58 -0300

    (chore) instalação e configuração do react router dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,1 +1,19 @@
+import { Routes, Route } from "react-router-dom";
 
+import Login from "./pages/Login";
+import DashBoard from "./pages/DashBoard";
+import Products from "./pages/Products";
+import Batches from "./pages/Batches";
+
+function App() {
+   return (
+      <Routes>
+         <Route path="/login" element={<Login />} />
+         <Route path="/dashboard" element={<DashBoard />} />
+         <Route path="/products" element={<Products />} />
+         <Route path="/batches" element={<Batches />} />
+      </Routes>
+   );
+}
+
+export default App;
---
+++ frontend/src/pages/Batches.tsx
@@ -0,0 +1,3 @@
+export default function Batches() {
+   return <h1>Lotes</h1>;
+}
